efaa188fe106ada6c9e0d3880e3376cf313a9ff6
DuTimDu <114092280+DuTimDu@users.noreply.github.com>
2024-06-07 13:54:17 +0200

    Update authentification.js

--- authentification.js
+++ authentification.js
@@ -4,8 +4,16 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Vérification des identifiants
-    if (username === 'mathieu' && password === 'zozo') {
+    const users = [
+        { username: 'mathieu', password: 'zozo' },
+        { username: 'alice', password: 'password1' },
+        { username: 'bob', password: 'password2' }
+    ];
+
+    const validUser = users.find(user => user.username === username && user.password === password);
+
+    if (validUser) {
+        localStorage.setItem('username', username); // Stocke le nom d'utilisateur
         window.location.href = 'home.html'; // Redirection vers la page d'accueil
     } else {
         document.getElementById('error-message').textContent = 'Identifiants incorrects';
