d3c84ed86e67e855b26d9f1794e1eb416da1cdf8
DuTimDu <114092280+DuTimDu@users.noreply.github.com>
2024-06-07 11:27:57 +0200

    intro

--- authentification.js
+++ authentification.js
@@ -1,35 +1,13 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const app = express();
-app.use(express.json());
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
 
-let users = [];
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-app.post('/users', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = { name: req.body.name, password: hashedPassword };
-        users.push(user);
-        res.status(201).send();
-    } catch {
-        res.status(500).send();
+    // Vérification des identifiants
+    if (username === 'mathieu' && password === 'zozo') {
+        window.location.href = 'home.html'; // Redirection vers la page d'accueil
+    } else {
+        document.getElementById('error-message').textContent = 'Identifiants incorrects';
     }
 });
-
-app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.name === req.body.name);
-    if (user == null) {
-        return res.status(400).send('Cannot find user');
-    }
-    try {
-        if(await bcrypt.compare(req.body.password, user.password)) {
-            res.send('Success');
-        } else {
-            res.send('Not Allowed');
-        }
-    } catch {
-        res.status(500).send();
-    }
-});
-
-app.listen(3000);
