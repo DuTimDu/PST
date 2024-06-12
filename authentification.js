17412d6d8983ef600f8e2121e38b619326f074be
DuTimDu <114092280+DuTimDu@users.noreply.github.com>
2024-06-12 05:11:31 +0200

    un truc qui marche

--- authentification.js
+++ authentification.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Ouvrir la base de données SQLite
 let db = new sqlite3.Database('data.db');
-
+uname= '';
 // Route pour gérer l'authentification
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
@@ -24,12 +24,37 @@ app.post('/login', (req, res) => {
             console.error(err.message);
             res.status(500).send('Erreur interne du serveur');
         } else if (row) {
+            uname = username;
             res.redirect('/home.html'); // Rediriger vers home.html si les identifiants sont corrects
         } else {
             res.status(401).send('Identifiants incorrects'); // Envoyer une réponse d'erreur si les identifiants sont incorrects
         }
     });
 });
+
+// Route pour enregistrer les sélections dans la base de données
+app.post('/saveSelections', (req, res) => {
+    const { userId= uname, projectId, hour, selected } = req.body;
+    const selections = req.body.selections;
+    // Connexion à la base de données
+    const db = new sqlite3.Database('database.db');
+
+    // Insertion de la sélection dans la base de données
+    db.serialize(() => {
+        const stmt = db.prepare('INSERT INTO selections (project) VALUES (?)');
+        selections.forEach(selection => {
+            stmt.run(selection);
+        });
+        stmt.finalize();
+    });
+
+    // Ferme la connexion à la base de données
+    db.close();
+
+    res.redirect('/'); // Redirige vers la page d'accueil
+});
+
+
 
 // Démarrer le serveur
 app.listen(port, () => {
